5fea0179f55d2b86f08d1f13d3e0054290d9903c
JUNGSANGYUN <sdsd98987@gmail.com>
2023-06-23 10:49:37 +0900

    회원 조회 API 개발

--- jpashop-v20210728/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop-v20210728/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,8 @@ import org.apache.coyote.http11.filters.SavedRequestInputFilter;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,9 +18,38 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+
+        List<Member> findMembers = memberService.findMembers();
+
+        List<MemberDto> collect = findMembers.stream().map(m -> new MemberDto(m.getName())).collect(Collectors.toList());
+
+        return new Result(collect.size(),collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
     //문제 : 파라미터로 엔티티를 받고 있다. 만약 엔티티가 변경 되면 문제 발생
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
@@ -38,7 +69,7 @@ public class MemberApiController {
             @PathVariable("id") Long id,
             @RequestBody @Valid UpdateMemberRequest request){
 
-        //수정 기능 개발시, 쿼리와 커맨드를 분리하면 유지보수에 장점이 있다. 그래서 update 기능에 반환값으로 member 보다 void, id 값 정도 사용
+        //수정 기능 개발시, 쿼리와 커맨드를 분리하면 유지보수에 장점이 있다. 그래서 update 기능에 반환값으로 member 보다 void, id
         memberService.update(id, request.getName());
         Member findMember = memberService.findOne(id);
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
